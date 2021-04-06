1955b9d672e24ea8df35f779e489b6bb7922a57f
john-reiner <johnreiner1990@gmail.com>
2021-04-06 09:26:20 -0400

    my solution o(n)

--- easy/sortedSquareArray.js
+++ easy/sortedSquareArray.js
@@ -1,15 +1,43 @@
+// function sortedSquaredArray(array) {
+
+//     let returnArray = []
+
+//     for (let i = 0; i < array.length; i++) {
+//         returnArray.push((array[i] * array[i]))
+//     }
+//     return returnArray.sort(function(a,b) {
+//         return a-b
+//     });
+
+// }
+
 function sortedSquaredArray(array) {
-    
-    let returnArray = []
+    let leftIndex = 0;
+    let rightIndex = array.length - 1;
+    let returnArray = new Array(array.length);
+    let indexCheck = rightIndex; 
+
+    if (leftIndex === rightIndex) returnArray[indexCheck] = array[leftIndex] * array[leftIndex];
 
     for (let i = 0; i < array.length; i++) {
-        returnArray.push((array[i] * array[i]))
+        if (Math.abs(array[leftIndex]) > Math.abs(array[rightIndex])) {
+            returnArray[indexCheck] = array[leftIndex] * array[leftIndex];
+            leftIndex ++;
+
+        } else {
+            returnArray[indexCheck] = array[rightIndex] * array[rightIndex];
+            rightIndex --;
+
+        };
+
+
+        indexCheck --;
     }
-    return returnArray.sort(function(a,b) {
-        return a-b
-    });
+
+    return returnArray
 }
 
-const array = [-1, -1, 2, 3, 3, 3, 4]
+
+const array = []
 
 console.log(sortedSquaredArray(array))
