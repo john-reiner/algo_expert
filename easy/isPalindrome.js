e90bf192211dffdbf91b1987df6431aaabfb616f
john-reiner <johnreiner1990@gmail.com>
2021-04-24 19:43:42 -0400

    my solution

---
+++ easy/isPalindrome.js
@@ -0,0 +1,26 @@
+var isPalindrome = function(x) {
+    let numStr = x.toString();
+
+    let pointerOne = 0;
+    let pointerTwo = numStr.length - 1;
+    for (let i = 0; i < numStr.length; i++) {
+        let leftValue = numStr[pointerOne];
+        let rightValue = numStr[pointerTwo];
+        if (leftValue !== rightValue) {
+            return false 
+        } else {
+            pointerOne ++
+            pointerTwo --
+        }
+    }
+    return true 
+};
+
+var x = 123456543216
+
+console.log(isPalindrome(x))
+
+// convert num to str 
+// loop over the string 
+// set up 2 pointers 
+// if the 2 pointers ever dont match, return false
