65f2c6e0a4355c1227170ae12fb248cd43d77d5e
john-reiner <johnreiner1990@gmail.com>
2021-04-26 18:48:36 -0400

    my solution

---
+++ easy/strStr.js
@@ -0,0 +1,42 @@
+var strStr = function(haystack, needle) {
+    if (needle.length > haystack.length) return -1
+    if (needle === "") {
+        return 0
+    } else {
+        for (let i = 0; i < haystack.length; i++) {
+            const haystackLetter = haystack[i];
+            if (haystackLetter === needle[0]) {
+                let checkIndex = i 
+                for (let j = 0; j < needle.length; j++) {
+                    const needleLetter = needle[j];
+                    if (needleLetter === haystack[checkIndex]) {
+                        checkIndex ++
+                        if (needle.length === (checkIndex - i) && checkIndex <= haystack.length) {
+                            return i 
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return -1
+};
+
+var haystack = "c"
+var needle = "c"
+
+console.log(strStr(haystack, needle))
+
+// if needle === ""
+//  return 0
+// else 
+//  loop over haystack 
+//  if haystack[i] === needle[0]
+//      set haystackIndex = i 
+//      loop over needle 
+//      if (needle[j] !== haystack[haystackIndex])
+//          break
+//      else 
+//          return true 
+
+
